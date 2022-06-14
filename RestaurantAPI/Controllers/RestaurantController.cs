1307dafa9763e86049811769fe174d996afdfd5f
AdrianN09 <adrnie129@gmail.com>
2022-06-14 22:04:21 +0200

    Added migrations, created entities and restaurant controller

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Data;
+using RestaurantAPI.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI.Controllers
+{
+
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restourants = _dbContext
+                .Restaurants
+                .ToList();
+            return Ok(restourants);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> Get([FromRoute] int id)
+        {
+            var restourant = _dbContext
+                .Restaurants
+                .FirstOrDefault(x => x.Id == id);
+
+            if(restourant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restourant);
+        }
+    }
+}
---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,63 @@
+﻿using RestaurantAPI.Data;
+using RestaurantAPI.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description ="KFC short for Kentucky Friend Chicken",
+                    ContactEmail ="contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Hot Chicken",
+                            Price = 10.20M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chiken Nuggets",
+                            Price =5.20M,
+                        }
+                    },
+                    Address = new Address()
+                    {
+                        City="Krakow",
+                        Street="Dluga 5",
+                        PostalCode="20231-2"
+                    }
+                }
+            };
+            return restaurants;
+        }
+    }
+}
