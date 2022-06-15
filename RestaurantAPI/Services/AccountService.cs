f61c96830d653e3679c5052282682677e52299a2
Adrian Nieckarz <adrian.nieckarz@outlook.com>
2022-06-15 19:07:46 +0200

    Added user registration

---
+++ RestaurantAPI/Services/AccountService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RestaurantAPI.Data;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Services
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUserDto dto);
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _dbContext;
+        private readonly IPasswordHasher<User> _passwordHasher;
+        public AccountService(RestaurantDbContext dbContext, IPasswordHasher<User> passowrdHasher)
+        {
+            _passwordHasher = passowrdHasher;
+            _dbContext = dbContext;
+        }
+
+        public void RegisterUser(RegisterUserDto dto)
+        {
+
+            var newUser = new User()
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateofBirth,
+                Nationality = dto.Nationality,
+                RoleId = dto.RoleId,
+            };
+
+            var hashedPassword =_passwordHasher.HashPassword(newUser, dto.Password);
+            newUser.PassworHash = hashedPassword;
+
+            _dbContext.Users.Add(newUser);
+            _dbContext.SaveChanges();
+            
+        }
+
+    }
+}
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -1,13 +1,19 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestaurantAPI.Data;
+using RestaurantAPI.Entities;
 using RestaurantAPI.Middleware;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
 using RestaurantAPI.Services;
 using System;
 using System.Collections.Generic;
@@ -28,11 +34,14 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation();
             services.AddDbContext<RestaurantDbContext>();
             services.AddScoped<RestaurantSeeder>();
             services.AddAutoMapper(this.GetType().Assembly);
             services.AddScoped<IRestaurantService, RestaurantService>();
+            services.AddScoped<IAccountService,AccountService>();
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+            services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
             services.AddScoped<IDishService, DishService>();
             services.AddScoped<ErrorHandlingMiddleware>();
             services.AddSwaggerGen();
